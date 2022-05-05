32914e6726eef7053af2865f1e3f01ad0a3db783
Azeez Samad <seshp@ymail.com>
2022-05-05 17:24:18 +0100

    add mobile navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Navbar.css'
 import {FaBars, FaTimes} from 'react-icons/fa'
 
 const Navbar = () => {
+const [nav, setNav] = useState(false);
+const handleClick = () => setNav(!nav); 
+     
   return (
     <div className="header">
         <div className="container">
             <h1>Crypt<span className="primary">Meta</span></h1>
-            <ul className="nav-menu">
+            <ul className={nav ? "nav-menu active" : "nav-menu"}>
                 <li>
                     <a href="/">Home</a>
                 </li>
@@ -24,8 +27,9 @@ const Navbar = () => {
             <div className="btn-group">
                 <button className="btn">Connect wallet</button>
             </div>
-            <div className="hamburger">
-                <FaBars size={20} color="#333"/>
+            <div className="hamburger" onClick={handleClick}>
+                {nav? (<FaTimes size={20} color="#333"/> ) : (<FaBars size={20} color="#333"/>)}
+                
             </div>
         </div>
       
